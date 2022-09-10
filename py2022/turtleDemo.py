79961889bd669d1744012ae946ebb8dd8018e792
jschung <jc7qxccit@gmail.com>
2022-09-10 16:11:01 +0800

    test upload to github

---
+++ py2022/turtleDemo.py
@@ -0,0 +1,17 @@
+import turtle
+import random
+
+# print("hello")
+win = turtle.Screen()
+
+tk = turtle.Turtle()
+turtle.colormode(255)
+for a in range(36):
+    r, g, b = random.randint(0, 255), random.randint(0, 255),random.randint(0, 255)
+    #print(r, g, b)
+    tk.color((r, g, b))
+
+    tk.circle(50)
+    tk.right(10)
+
+win.exitonclick()
